33dd99c12b1c12cd7070f91e8421ff9a8805ca01
ilviirgroup <ilviirgroup@gmail.com>
2021-06-22 21:12:30 +0600

    urls

--- tutorial_app/urls.py
+++ tutorial_app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from tutorial_app import views
 
 urlpatterns = [
-	url(r'^language/$', views.category_list),
+	url(r'^categories/$', views.category_list),
 	url(r'^language/(?P<pk>[0-9]+)$', views.category_detail),
 ]
